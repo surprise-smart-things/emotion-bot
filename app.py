0ba364225ad978bece158c34bab6bad1be84fc46
Allen Bijo <allenbijot@gmail.com>
2024-04-20 18:03:49 +0530

    fixed bugs in video recording

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
+recordings_dir = os.path.join(app.root_path, 'videos')
 
 
 @app.route('/')
@@ -13,6 +14,17 @@ def home():
 def record():
     directory = os.path.join(app.root_path, 'videos')
     return render_template('record.html')
+
+
+@app.route('/save-video', methods=['POST'])
+def save_video():
+    if 'video' in request.files:
+        video = request.files['video']
+        if video:
+            video_path = os.path.join(recordings_dir, 'recording.mp4')
+            video.save(video_path)
+            return 'Video saved successfully', 200
+    return 'Error saving video', 400
 
 
 if __name__ == '__main__':
