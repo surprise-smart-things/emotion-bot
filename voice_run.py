a114b9fe88895690eed814e8ec5557aed6524978
SiddhiMarri <135405913+SiddhiMarri@users.noreply.github.com>
2024-04-21 23:35:41 +0530

    Updated voice_run.py to return name of emotion

--- voice_run.py
+++ voice_run.py
@@ -11,23 +11,24 @@ import numpy as np
 
 
 def runner(filepath):
-	y, sr = librosa.load(filepath)
-	zcr = librosa.feature.zero_crossing_rate(y)
-	average_zcr = np.mean(zcr)
-	mfc = librosa.feature.mfcc(y=y, sr=sr)
-	avg_mfc = np.mean(mfc)
-	speccentroid = librosa.feature.spectral_centroid(y=y, sr=sr)
-	avg_speccentroid = np.mean(speccentroid)
-	specrolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-	avg_specrolloff = np.mean(specrolloff)
-	tempo = librosa.feature.tempo(y=y, sr=sr)[0]
-	beats = librosa.onset.onset_detect(y=y, sr=sr).sum()
-	model1 = load_model('Backup.h5')
+	y,sr=librosa.load(filepath)
+   	zcr=librosa.feature.zero_crossing_rate(y)
+	average_zcr=np.mean(zcr)
+	mfc=librosa.feature.mfcc(y=y, sr=sr)
+	avg_mfc=np.mean(mfc)
+	speccentroid=librosa.feature.spectral_centroid(y=y, sr=sr)
+	avg_speccentroid=np.mean(speccentroid)
+	specrolloff=librosa.feature.spectral_rolloff(y=y, sr=sr)
+	avg_specrolloff=np.mean(specrolloff)
+	tempo=librosa.feature.tempo(y=y,sr=sr)[0]
+	beats=librosa.onset.onset_detect(y=y, sr=sr).sum()
+	model1= load_model('Backup.h5')
 	all_features = np.array([average_zcr, avg_mfc, avg_speccentroid, avg_specrolloff, tempo, beats])
 	all_features = np.expand_dims(all_features, axis=0)
-	prediction = model1.predict(all_features, verbose=0)
-	emotion = prediction.argmax()
-	return emotion
+	prediction= model1.predict(all_features)
+	emotion=prediction.argmax()
+	dictt={0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 4:'Neutral', 5:'Sad'}
+	return dictt[emotion]
 
 
 if __name__ == '__main__':
