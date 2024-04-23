2eee8ab60da3f68fde63da1037dba111c32bf771
Allen Bijo <allenbijot@gmail.com>
2024-04-23 07:13:04 +0530

    big changes to voice model

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from face_run import run_emotion_detection
+from face_run import run_emotion_detection as face_runner
+from voice_run import runner as voice_runner
+from moviepy.editor import VideoFileClip, AudioClip
 import os
 
 
+def extract_audio(input_file, output_file):
+	video = VideoFileClip(input_file)
+	audio = video.audio
+	audio.write_audiofile(output_file)
+
+	print(f"Audio successfully extracted to {output_file}")
+
+
 def run(path):
-	return run_emotion_detection(path), {'angry': 0.9609064598416173, 'sad': 0.32430961693442145}, 'Happy'
-	# return path, {'angry': 0.9609064598416173, 'sad': 0.32430961693442145}, 'Happy'
+	face_dict = face_runner(path)
+	face_value = max(face_dict, key=face_dict.get)
+	audio_path = 'audios/' + (path.split('.')[0] + '.mp3').split('/')[1]
+	extract_audio(path, audio_path)
+	voice_value = voice_runner(audio_path)
+	final_value = face_value if face_value == voice_value else 'Undetermined'
+	return face_value, voice_value, final_value
+
 
 if __name__ == '__main__':
 	for file in os.listdir('videos'):
-		print(run(f'videos/{file}'))
-		break
+		print(file, run(f'videos/{file}'))
+		# break
+
+	# extract_audio('videos/recording2.mp4', 'audios/recording2.mp3')
